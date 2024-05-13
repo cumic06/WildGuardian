4432c3488cbf3cb449ba4fdd88dd41821e60247f
bch6224 <92432064+cumic06@users.noreply.github.com>
2024-05-13 15:51:25 +0900

    AllPrefab & MonsterSpawn
    몬스터 프리팹화 MonsterSpawnSystem 몬스터 스폰위치 카메라 밖으로 소환하게 만드는중

--- Assets/02.System/SpawnMonsterSystem.cs
+++ Assets/02.System/SpawnMonsterSystem.cs
@@ -5,11 +5,36 @@ using UnityEngine;
 public class SpawnMonsterSystem : MonoSingleton<SpawnMonsterSystem>
 {
     [SerializeField] private SpawnMonsterData spawnMonsterData;
+    [SerializeField] private float spawnTime;
+    private float currentSpawnTime;
+
+    private void Start()
+    {
+        UpdateSystem.Instance.AddFixedUpdateAction(SpawnSystem);
+    }
+
+    private void SpawnSystem()
+    {
+        currentSpawnTime += Time.deltaTime;
+
+        if (IsCanSpawn())
+        {
+            currentSpawnTime = 0;
+            Vector2 spawnPos = (Vector2)Player.Instance.transform.position;
+            Debug.Log(spawnPos);
+            SpawnMonster(spawnPos);
+        }
+    }
 
     public void SpawnMonster(Vector2 spawnPos)
     {
         int randomIndex = Random.Range(0, spawnMonsterData.spawnMonsterInfo.monsterList.Count);
 
         Instantiate(spawnMonsterData.spawnMonsterInfo.monsterList[randomIndex], spawnPos, Quaternion.identity);
+    }
+
+    private bool IsCanSpawn()
+    {
+        return currentSpawnTime >= spawnTime;
     }
 }
